19cbea9d038565297e7fe82c4d3e464b43b8c030
HugoCleret <hugocleret@hotmail.fr>
2019-09-19 16:48:54 +0200

    Ajoutez des fichiers projet.

---
+++ ModelMetier/Film.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelMetier
+{
+    class Film
+    {
+        public string NbEntrees { get; set; }
+        public string PhotoFilm { get; set; }
+
+        public string TitreFilm { get; set; }
+    }
+
+    public void AjouterActeur()
+    {
+
+    }
+}
